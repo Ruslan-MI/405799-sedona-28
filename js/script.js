4100640ca6f359cfe151f3cd83ae78e029510ef1
Ruslan-MI <64783699+Ruslan-MI@users.noreply.github.com>
2020-06-29 20:01:35 +0300

    Небольшие правки и добавления
    При первом открытии окна формы, любым из трёх способов (клик, "таб", "шифт-таб"):
    - включается автозаполнение;
    - фокус переносится на поле даты въезда, выделяется его содержимое.
    "Шифт-таб", при открытом окне, сохраняет порядок фокусировки.
    Добавлена возможность менять количество людей клавишами "вверх" и "вниз".

--- js/script.js
+++ js/script.js
@@ -23,22 +23,77 @@ try {
 searchPopup.classList.add("visually-hidden");
 searchPopup.classList.add("narrowing");
 
-searchPopupOpen.addEventListener("click", function (evt) {
+searchPopupOpen.addEventListener("click", function () {
   if (searchPopup.classList.contains("visually-hidden")) {
     searchPopup.classList.remove("visually-hidden");
-  };
-  searchPopup.classList.toggle("narrowing");
-  searchPopup.classList.toggle("expansion");
-  if (searchPopup.classList.contains("expansion")) {
-    dateInInput.focus();
-    if (adults) {
+    if (isStorageSupport) {
       adultsInput.value = localStorage.getItem("adults");
+      if (adultsInput.value < 1) {
+        adultsInput.value = 2;
+      };
       childrenInput.value = localStorage.getItem("children");
+      if (childrenInput.value < 1) {
+        childrenInput.value = 0;
+      };
     } else {
       adultsInput.value = 2;
       childrenInput.value = 0;
     };
   };
+  searchPopup.classList.toggle("narrowing");
+  searchPopup.classList.toggle("expansion");
+  if (searchPopup.classList.contains("expansion")) {
+    dateInInput.focus();
+    dateInInput.select();
+  };
+});
+
+dateInInput.addEventListener("focus", function () {
+  if (searchPopup.classList.contains("visually-hidden")) {
+    searchPopup.classList.remove("visually-hidden");
+    if (isStorageSupport) {
+      adultsInput.value = localStorage.getItem("adults");
+      if (adultsInput.value < 1) {
+        adultsInput.value = 2;
+      };
+      childrenInput.value = localStorage.getItem("children");
+      if (childrenInput.value < 1) {
+        childrenInput.value = 0;
+      };
+    } else {
+      adultsInput.value = 2;
+      childrenInput.value = 0;
+    };
+  };
+  if (searchPopup.classList.contains("narrowing")) {
+    searchPopup.classList.remove("narrowing");
+    searchPopup.classList.add("expansion");
+  }
+});
+
+searchFormSubmit.addEventListener("focus", function () {
+  if (searchPopup.classList.contains("visually-hidden")) {
+    searchPopup.classList.remove("visually-hidden");
+    if (isStorageSupport) {
+      adultsInput.value = localStorage.getItem("adults");
+      if (adultsInput.value < 1) {
+        adultsInput.value = 2;
+      };
+      childrenInput.value = localStorage.getItem("children");
+      if (childrenInput.value < 1) {
+        childrenInput.value = 0;
+      };
+    } else {
+      adultsInput.value = 2;
+      childrenInput.value = 0;
+    };
+  };
+  if (searchPopup.classList.contains("narrowing")) {
+    searchPopup.classList.remove("narrowing");
+    searchPopup.classList.add("expansion");
+    dateInInput.focus();
+    dateInInput.select();
+  }
 });
 
 window.addEventListener("keydown", function (evt) {
@@ -51,48 +106,60 @@ window.addEventListener("keydown", function (evt) {
   };
 });
 
-dateInInput.addEventListener("focus", function (evt) {
-  if (searchPopup.classList.contains("visually-hidden")) {
-    searchPopup.classList.remove("visually-hidden");
-  };
-  if (searchPopup.classList.contains("narrowing")) {
-    searchPopup.classList.remove("narrowing");
-    searchPopup.classList.add("expansion");
-  }
-});
-
-searchFormSubmit.addEventListener("focus", function (evt) {
-  if (searchPopup.classList.contains("visually-hidden")) {
-    searchPopup.classList.remove("visually-hidden");
-  };
-  if (searchPopup.classList.contains("narrowing")) {
-    searchPopup.classList.remove("narrowing");
-    searchPopup.classList.add("expansion");
-  }
-});
-
-adultsMinus.addEventListener("click", function (evt) {
+adultsMinus.addEventListener("click", function () {
   if (adultsInput.value > 1) {
     adultsInput.value--;
   }
 });
 
-adultsPlus.addEventListener("click", function (evt) {
+adultsInput.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 40) {
+    if (adultsInput.value > 1) {
+      adultsInput.value--;
+    };
+  };
+});
+
+adultsPlus.addEventListener("click", function () {
   if (adultsInput.value < 9) {
     adultsInput.value++;
   }
 });
 
-childrenMinus.addEventListener("click", function (evt) {
+adultsInput.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 38) {
+    if (adultsInput.value < 9) {
+      adultsInput.value++;
+    };
+  };
+});
+
+childrenMinus.addEventListener("click", function () {
   if (childrenInput.value > 0) {
     childrenInput.value--;
   }
 });
 
-childrenPlus.addEventListener("click", function (evt) {
+childrenInput.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 40) {
+    if (childrenInput.value > 0) {
+      childrenInput.value--;
+    };
+  };
+});
+
+childrenPlus.addEventListener("click", function () {
   if (childrenInput.value < 9) {
     childrenInput.value++;
   }
+});
+
+childrenInput.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 38) {
+    if (childrenInput.value < 9) {
+      childrenInput.value++;
+    };
+  };
 });
 
 searchForm.addEventListener("submit", function (evt) {
